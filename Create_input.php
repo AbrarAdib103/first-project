99cf2f15bc84e2319319678041d6a13f0bd8cad7
AbrarAdib103 <148997635+AbrarAdib103@users.noreply.github.com>
2024-02-15 11:53:39 +0600

    Enter Input

---
+++ Create_input.php
@@ -0,0 +1,17 @@
+<h1>Create Record</h1>
+
+
+
+<form method=get action=create_result.php>
+
+	product_id: <input type=text name=product_id> <br>
+
+	<p>
+
+	product_name: <input type=text name=product_name> <br>
+
+	<p>
+
+	<input type=submit value=Insert>
+
+</form>
